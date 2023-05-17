161dd697da015a49fe757c38876fd5f8e87f1b88
Yana <yanazvukova@gmail.com>
2023-05-17 18:20:37 +0300

    .

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 
 def computer_network():
-    graph = []
+    graph = [set()] * 1347
     with open('alg_rf7.txt') as input_file:
-        while True:
-            adj_lst = [int(i) for i in input_file.readline().split()]
+        for line in input_file.readlines():
+            adj_lst = list(map(int, line.split()))
             if adj_lst:
-                adj_lst.pop(0)
-                graph.append(set(adj_lst))
+                vertex1 = adj_lst.pop(0)
+                graph[vertex1] = set(adj_lst)
+                for vertex2 in adj_lst:
+                    graph[vertex2].add(vertex1)
             else:
                 break
     return graph
+
+
+def compute_resilience(graph, vertices):
+    for vertex in vertices:
+        graph[vertex] = set()
+
 
 
 if __name__ == '__main__':
