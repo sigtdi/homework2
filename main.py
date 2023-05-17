bd8695e282337c529a22d7445f40fe9d1365f860
Yana <yanazvukova@gmail.com>
2023-05-17 18:43:42 +0300

    .

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def computer_network():
             adj_lst = list(map(int, line.split()))
             if adj_lst:
                 vertex1 = adj_lst.pop(0)
-                graph[vertex1] = set(adj_lst)
+                graph[vertex1].update(adj_lst)
                 for vertex2 in adj_lst:
                     graph[vertex2].add(vertex1)
             else:
@@ -15,8 +15,32 @@ def computer_network():
 
 
 def compute_resilience(graph, vertices):
+    connect_comp = []
+    n = len(graph)
     for vertex in vertices:
         graph[vertex] = set()
+        for adj_lst in graph:
+            if vertex in adj_lst:
+                adj_lst.remove(vertex)
+        visited = [False] * n
+        answer = 0
+        for i in range(n):
+            temp_size = 0
+            if visited[i]:
+                continue
+            visited[i] = True
+            queue = [i]
+            temp_size += 1
+            while queue:
+                v = queue.pop()
+                for to in graph[v]:
+                    if not visited[to]:
+                        temp_size += 1
+                        visited[to] = True
+                        answer = max(answer, temp_size)
+                        queue.append(to)
+        connect_comp.append(answer)
+    return connect_comp
 
 
 
