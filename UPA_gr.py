746b14d703a6d3fda1282f05c45e8a066abd6f7c
res <triw.onlie@yandex.ru>
2023-05-18 23:54:12 +0300

    func to build UPA

--- UPA_gr.py
+++ UPA_gr.py
@@ -1,6 +1,32 @@
-from alg_dpa_trial import DPATrial
+from alg_upa_trial import UPATrial
 
-def gen_UPA()
+def gen_UPA(n, m):
+    '''
+    :параметр n: общее количество вершин в создаваемом графе
+    :параметр m: (m<n) количество вершин, к которым подсоединяется каждая вновь созданная вершина
+    :возвращает: случайный граф
+
+    Использует класс для инкапсуляции оптимизированных испытаний UPATrial
+    '''
+
+    # инициализируем граф и создаем полносвязный граф на m вершинах
+    upaG = [set()] * n
+    nodes_of_g_complete = set(range(m))
+    for i in range(m):
+        upaG[i] = nodes_of_g_complete.difference({i})
+
+    # создаем объект класса UPATrial для проведения случайных испытаний;
+    # для каждой добавляемой вершины проводим испытания и соединем ее с новыми соседями, полученными в результате испытаний
+    upatrial = UPATrial(m)
+    for v in range(n - m + 1, n):
+        new_node_neighbors = upatrial.run_trial(m)
+        for u in new_node_neighbors:
+            upaG[v].add(u)
+            upaG[u].add(v)
+
+    return upaG
+
+
 
 
 if __name__ == '__main__':
