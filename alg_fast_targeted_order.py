a7d14b1e92b3b199890221862c4970c3200c04bb
Kate Pavlova <triw.onlie@yandex.ru>
2023-05-21 10:11:00 +0300

    комментирование

--- alg_fast_targeted_order.py
+++ alg_fast_targeted_order.py
@@ -1,5 +1,5 @@
 '''
-    Эффективная реализация удаление вершин с учетом структуры графа
+    Эффективная реализация удаления вершин с учетом структуры графа
     Удаление происходит в порядке убывания степеней вершин, находящихся в графе
 '''
 
@@ -8,23 +8,34 @@ from graph_operations import *
 
 
 def fast_targeted_order(ugraph):
+    '''
+        :параметр ugraph: неориентированный граф
+        :возвращает: упорядоченный список вершин order в порядке убывания их степеней
+
+        Использует вспомогательные функции для работы с графом
+    '''
+    # создаем копию графа
     new_graph = copy_graph(ugraph)
     num_nodes = len(new_graph)
+    # создаем список, содержащий информацию о степенях вершин, где  k-тый элемент содержит
+    # множество узлов со степенью k
     degree_sets = [set([]) for _ in range(num_nodes)]
     for node in range(num_nodes):
         degree_node = len(new_graph[node])
         degree_sets[degree_node].add(node)
-
+    # инициализируем пустой список для хранения удаляемых вершин
     order = []
-
+    # перебираем все элементы degree_sets в порядке убывания степени k.
     for k in range(num_nodes - 1, -1, -1):
+        # Далее поочередно удаляем все вершины из этого множества
+        # и параллельно обновляем множества из degree_sets
         while degree_sets[k]:
             node = degree_sets[k].pop()
             for neighbor in new_graph[node]:
                 d_neighbor = len(new_graph[neighbor])
                 degree_sets[d_neighbor].remove(neighbor)
                 degree_sets[d_neighbor - 1].add(neighbor)
-
+            # добавляем найденную вершину в order и удаляем ее из графа
             order.append(node)
             delete_node(new_graph, node)
 
--- alg_targeted_order.py
+++ alg_targeted_order.py
@@ -1,5 +1,5 @@
 '''
-    Не очень эффективная реализация удаление вершин с учетом структуры графа
+    Не очень эффективная реализация удалениея вершин с учетом структуры графа
     Удаление происходит в порядке убывания степеней вершин, находящихся в графе
 '''
 
@@ -8,18 +8,29 @@ from graph_operations import *
 
 
 def targeted_order(ugraph):
+    '''
+        :параметр ugraph: неориентированный граф
+        :возвращает: упорядоченный список вершин order в порядке убывания их степеней
+
+        Использует вспомогательные функции для работы с графом
+    '''
+    # создаем копию графа
     new_graph = copy_graph(ugraph)
 
+    # инициализируем пустой список для хранения удаляемых вершин
     order = []
     num_nodes = len(new_graph)
     num_nodes_order = 0
+    # пока в графе есть вершины, удаляем их
     while num_nodes != num_nodes_order:
+        # Находит узел с максимальной степенью.
+        # Если таковых несколько, то выбирает любой.
         max_degree = -1
         for node in range(num_nodes):
             if new_graph[node] is not None and len(new_graph[node]) > max_degree:
                 max_degree = len(new_graph[node])
                 max_degree_node = node
-
+        # Удаляет узел с максимальной степенью и все инцидентные ребра из графа.
         delete_node(new_graph, max_degree_node)
         order.append(max_degree_node)
         num_nodes_order += 1
